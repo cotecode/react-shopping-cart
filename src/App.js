24b4aad67e6231b73b06a8afeea290c70dff1eb7
nico <nicoparulian2@gmail.com>
2021-08-31 11:28:57 +0700

    first

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="grid-container">
       <header>
-        <a href="/">React Shopping Cart !</a>
+        <a href="/">React Shopping Cart</a>
       </header>
       <main>Product List</main>
       <footer>All right is reserved</footer>
